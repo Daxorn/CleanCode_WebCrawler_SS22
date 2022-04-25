76bfece539d1ae5fd4cde899996a7d3eeb7b5609
baciiu <albaciu@edu.aau.at>
2022-04-25 22:02:55 +0200

    created test classes

---
+++ src/test/java/TranslatorTest.java
@@ -0,0 +1,39 @@
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+
+public class TranslatorTest {
+
+    private Translator translator;
+
+    @BeforeEach
+
+    public void setUp(){
+        translator = new Translator();
+    }
+
+    @Test
+    public void getTextTranslated() throws IOException {
+
+        String englishString = "Hello World!";
+        String germanString = "Hallo Welt!";
+
+        String english = "english";
+        String german = "german";
+
+        assertEquals(germanString, translator.getTranslation(englishString,english,german));
+        assertEquals(englishString, translator.getTranslation(germanString,german,english));
+    }
+
+    @AfterAll
+    public void tearDown(){
+        translator = null ;
+    }
+}
+
+
